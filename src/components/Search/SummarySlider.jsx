56c68d715349b446d6bd9c5dbf3e021583c80093
TreworReznik <kalinichenko.ilya@gmail.com>
2024-03-23 20:06:47 +0300

    Slider

--- src/components/Search/SummarySlider.jsx
+++ src/components/Search/SummarySlider.jsx
@@ -7,278 +7,176 @@ import styles from '../../styles/SummarySlider.module.css';
 function SummarySlider(props){
 
     return(
-        <div id={styles['slider_container']}>
+        <div className={styles.slider_container}>
             <div className={styles.left_part}>
-                <ul>
-                    <li>Период</li>
-                    <li>Всего</li>
-                    <li>Всего</li>
-                </ul>
+                    <p>Период</p>
+                    <p>Всего</p>
+                    <p>Риски</p>
             </div>
-            <Slider {...settingsSlider}>
-                <div className={styles.box} style={{ width: 140 ,height: 158}} >
-                    <div className={styles.list_column}>
+            <Slider  {...settingsSlider}>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                            <h4>17.09.2021</h4>
-                            <h4>3</h4>
-                            <h4>5</h4>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div className={styles.list_column}>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div className={styles.list_column}>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div className={styles.list_column}>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}} >
-                    <div>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
-                    </div>
-
-                </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div>
-                        <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
-                    </div>
-
-                </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div>
-                        <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
-                    </div>
-
-                </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div>
-                        <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
-                </div><div className={styles.box} style={{ width: 140 ,height: 158}}>
-                <div>
-                    <span></span>
-                    <ul>
-                        <li>17.09.2021</li>
-                        <li>3</li>
-                        <li>5</li>
-                    </ul>
                 </div>
-            </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div>
+                <div className={styles.box} >
+                    <div className={styles.content_box} >
                         <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
+                        <div className={styles.content_box_text}>
+                            <p>17.09.9999</p>
+                            <p>25</p>
+                            <p>6</p>
+                        </div>
                     </div>
-                </div><div className={styles.box} style={{ width: 140 ,height: 158}}>
-                <div>
-                    <span></span>
-                    <ul>
-                        <li>17.09.2021</li>
-                        <li>3</li>
-                        <li>5</li>
-                    </ul>
                 </div>
-            </div>
-                <div className={styles.box} style={{ width: 140 ,height: 158}}>
-                    <div>
-                        <span></span>
-                        <ul>
-                            <li>17.09.2021</li>
-                            <li>3</li>
-                            <li>5</li>
-                        </ul>
-                    </div>
-                </div><div className={styles.box} style={{ width: 140 ,height: 158}}>
-                <div>
-                    <span></span>
-                    <ul>
-                        <li>17.09.2021</li>
-                        <li>3</li>
-                        <li>5</li>
-                    </ul>
-                </div>
-            </div><div className={styles.box} style={{ width: 140 ,height: 158}}>
-                <div>
-                    <span></span>
-                    <ul>
-                        <li>17.09.2021</li>
-                        <li>3</li>
-                        <li>5</li>
-                    </ul>
-                </div>
-            </div><div className={styles.box} style={{ width: 140 ,height: 158}}>
-                <div>
-                    <span></span>
-                    <ul>
-                        <li>17.09.2021</li>
-                        <li>3</li>
-                        <li>5</li>
-                    </ul>
-                </div>
-            </div><div className={styles.box} style={{ width: 140 ,height: 158}}>
-                <div>
-                    <span></span>
-                    <ul>
-                        <li>17.09.2021</li>
-                        <li>3</li>
-                        <li>5</li>
-                    </ul>
-                </div>
-            </div>
-
-
-
-
-
-
 
             </Slider>
+
 
         </div>
     )
--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,3 +1,4 @@
+import styles from '../styles/SummarySlider.module.css'
 
 export const login_url = 'https://gateway.scan-interfax.ru/api/v1/account/login';
 
@@ -7,25 +8,45 @@ export  const settingsSlider = {
 
     infinite: false,
     speed: 500,
-    slidesToShow: 4,
+    slidesToShow: 8,
     slidesToScroll: 8,
     initialSlide: 0,
+    className: `${styles.slick_slider}`,
+    arrows: true,
     responsive: [
+        {
+            breakpoint: 1500,
+            settings: {
+                slidesToShow: 8,
+                slidesToScroll: 8,
+                infinite: false
+            }
+
+        },
+        {
+            breakpoint: 1280,
+            settings: {
+                slidesToShow: 7,
+                slidesToScroll: 7,
+                infinite: false,
+            }
+        },
+
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-
+                slidesToShow: 6,
+                slidesToScroll: 6,
+                infinite: false,
             }
         },
         {
-            breakpoint: 600,
+            breakpoint: 912,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                initialSlide: 2
+                slidesToShow: 5,
+                slidesToScroll: 5,
+                initialSlide: 2,
+                infinite: false,
             }
         },
         {
